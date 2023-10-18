ac4b19bfe64b0171b0e00ba0be1e8f9a26146e72
eringorishek <130519405+eringorishek@users.noreply.github.com>
2023-10-17 19:07:08 -0500

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS healthinsurance;
+CREATE TABLE healthinsurance (
+  	age FLOAT,
+  	sex FLOAT,
+ 	weight INT,
+ 	bmi FLOAT,
+	no_of_dependents INT,
+	smoker INT,
+	bloodpressure INT,
+	diabetes INT,
+	regular_ex INT,
+	bin_claim VARCHAR(255)
+);
